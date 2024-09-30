54254b1177f4b3265fd619a6d48e6a0a7903ca41
Mef1s <nikt988@gmail.com>
2024-09-30 20:52:07 +0200

    update

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -20,10 +20,65 @@ const Home = () => {
   return (
     <div id="Home">
             <div className="info-text">
-        <h2>Ośrodek</h2>
+        <h1>Ośrodek</h1>
         <p>Ośrodek Rewalidacyjno - Wychowawczy w Giemlicach jest publiczną, specjalistyczną placówką oświatową prowadzoną przez Gminę Cedry Wielkie. Ośrodek Rewalidacyjno - Wychowawczy w Giemlicach umożliwia realizację obowiązku szkolnego dzieciom i młodzieży niepełnosprawnej intelektualnie w stopniu umiarkowanym i znacznym ze sprzężeniami oraz w stopniu głębokim a także dzieciom ze zdiagnozowanym autyzmem, w wieku 3 do 25 lat, potrzebujących kompleksowej, wieloprofilowej pomocy.</p>
       </div>
-      <div id="container">
+      <div class="button-row">
+    <div class="ico-box bg-gray-light">
+        <div class="icon">
+            <span class="icon-document"><span class="sr-only">&amp;</span></span>
+        </div>
+        <h2 class="info-text">Placówka</h2>
+        <div class="des">
+            <p>o nas</p>
+        </div>
+        <div class="link-holder">
+            <a class="link-more" href="placowka.html#dokumenty">DOWIEDZ SIĘ WIĘCEJ</a>
+        </div>
+    </div>
+
+
+    <div class="ico-box bg-gray-light">
+        <div class="icon">
+            <span class="icon-document"><span class="sr-only">&amp;</span></span>
+        </div>
+        <h2 class="info-text">Dokumenty</h2>
+        <div class="des">
+            <p>Dokumenty konieczne do przyjęcia dziecka</p>
+        </div>
+        <div class="link-holder">
+            <a class="link-more" href="placowka.html#dokumenty">DOWIEDZ SIĘ WIĘCEJ</a>
+        </div>
+    </div>
+
+    <div class="ico-box bg-gray-light">
+        <div class="icon">
+            <span class="icon-document"><span class="sr-only">&amp;</span></span>
+        </div>
+        <h2 class="info-text">WWR</h2>
+        <div class="des">
+            <p>Wczesne wspomaganie rozwoju</p>
+        </div>
+        <div class="link-holder">
+            <a class="link-more" href="placowka.html#dokumenty">DOWIEDZ SIĘ WIĘCEJ</a>
+        </div>
+    </div>
+
+    <div class="ico-box bg-gray-light">
+        <div class="icon">
+            <span class="icon-document"><span class="sr-only">&amp;</span></span>
+        </div>
+        <h2 class="info-text">Praca</h2>
+        <div class="des">
+            <p>Cele i metody pracy</p>
+        </div>
+        <div class="link-holder">
+            <a class="link-more" href="placowka.html#dokumenty">DOWIEDZ SIĘ WIĘCEJ</a>
+        </div>
+    </div>
+</div>
+
+<div id="container">
       <Slider {...settings}>
         <div>
           <img src={slide1} id="SImage"/>
@@ -35,6 +90,19 @@ const Home = () => {
         </div>
       </Slider>
       </div>
+
+      <div class="contact-row">
+    <div class="contact-box">
+       <h4 class='info-text'>Giemlice 1<br></br>83-022 Suchy Dąb</h4>
+    </div>
+    <div class="contact-box">
+        <h4 class='info-text'>Placówka: +48 58 305-43-27<br></br>Dyrektor: +48 501-125-190</h4>
+    </div>
+    <div class="contact-box">
+      <h4 class='info-text'><a href="mailto:orw.giemlice@wp.pl">orw.giemlice@wp.pl</a></h4>
+    </div>
+</div>
+
     </div>
   );
 };
